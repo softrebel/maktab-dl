a80ad125bb0771a9168bbd2382777613cb9742b9
softrebel <sh.mohammad66@yahoo.com>
2025-01-22 04:03:13 +0330

    change package info

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="maktab-dl",
     version="0.1.0",
-    description="A simple command line tool to download videos",
-    author="Your Name",
-    author_email="your.email@example.com",
+    description="A simple downloader videos from maktabkhooneh",
+    author="Mohammadreza Shaghouzi",
+    author_email="sh.mohammad66@gmail.com.com",
     packages=find_packages(),
     install_requires=[
-        "httpx>=0.28.1",
-        "pydantic>=2.10.5",
-        "lxml>=5.3.0",
-        "click>=8.1.8",
-        "tqdm>=4.67.1",
+        "httpx",
+        "pydantic",
+        "lxml",
+        "click",
+        "tqdm",
     ],
     entry_points={
         "console_scripts": [
